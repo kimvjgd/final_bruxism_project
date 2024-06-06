0cf094e164f2db1b66688585305ecd79f6bafff9
kimvjgd <kimvjgd@naver.com>
2024-06-06 23:55:53 +0900

    new app

---
+++ firmware/STM32_BLUENRG/Bruxism_project/Core/Inc/app_bluenrg.h
@@ -0,0 +1,14 @@
+/*
+ * app_bluenrg.h
+ *
+ *  Created on: Dec 7, 2021
+ *      Author: hussamaldean
+ */
+
+#ifndef INC_APP_BLUENRG_H_
+#define INC_APP_BLUENRG_H_
+
+
+void bluenrg_init(void);
+void bluenrg_process(void);
+#endif /* INC_APP_BLUENRG_H_ */
